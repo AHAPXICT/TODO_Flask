be25a0b0b2c23efbc591d1f8794b1b1a8faa6c5a
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-01-30 12:37:52 +0200

    Update created_at validator in Todo.

--- flask-server/todo/todo/models.py
+++ flask-server/todo/todo/models.py
@@ -10,16 +10,24 @@ class Todo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(80), index=True, nullable=False)
-    body = db.Column(db.Text, nullable=True)
+    body = db.Column(db.Text, default='', nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    # slug = db.Column(db.String, default='', unique=True, nullable=False)
 
     @validates('created_at')
     def validates_created_at(self, key, value):
-        if self.created_at:
+        if self.created_at or value:
             raise ValueError("Created_at cannot be modified.")
 
         return value
+
+    # @validates('created_at')
+    # def validates_created_at(self, key, value):
+    #     if self.created_at:
+    #         raise ValueError("Created_at cannot be modified.")
+    #
+    #     return value
 
     @validates('updated_at')
     def validates_updated_at(self, key, value):
@@ -27,9 +35,9 @@ class Todo(db.Model):
             raise ValueError("Value for updated_at must be a datetime.")
         return value
 
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+    # def __init__(self, title, body=''):
+    #     self.title = title
+    #     self.body = body
 
     def __repr__(self):
-        return f'<Todo {self.id}, {self.body}>'
+        return f'<Todo {self.id}, {self.title}>'
