7359199cfede2fe49b4cc32c4ac6ef7193bfec24
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-02-04 17:01:51 +0200

    add cors for app

--- flask-server/todo/__init__.py
+++ flask-server/todo/__init__.py
@@ -8,6 +8,13 @@ from config import config
 db = SQLAlchemy()
 
 
+def _access_control(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+
 def register_blueprints(app):
     from .api import api_v1
 
@@ -18,8 +25,10 @@ def create_app(config_name: str):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    app.after_request(_access_control)
 
     db.init_app(app)
+
 
     register_blueprints(app)
 
